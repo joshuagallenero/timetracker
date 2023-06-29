9effe124b3f02f7fcec1d9cb0ca01d41f9be8e0b
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Add register and login viewsets for auth endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,11 @@
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
+from rest_framework import permissions, status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 from .serializers import (
+    RegisterUserSerializer,
     UserSerializer,
 )
 
@@ -13,3 +18,39 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = serializer_class.Meta.model.objects.all().order_by("-date_joined")
     permission_classes = [permissions.IsAuthenticated]
+
+
+class RegisterUserAPIView(viewsets.generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterUserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            data={**serializer.data, "token": token.key},
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response(
+            {
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "token": token.key,
+            }
+        )
