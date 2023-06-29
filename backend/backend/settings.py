d325ae53b3b22d6a0049a2e0b6403fe4dda668db
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Remove pagination settings from django rest framework settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,8 +134,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 1,
 }
 
 # CORS settings
