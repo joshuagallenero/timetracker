764175bfb8338de0d3c243227817a9338183b2fc
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Add component for logged tasks for tracking

---
+++ frontend/src/pages/home/RecordsList.jsx
@@ -0,0 +1,185 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { Button } from 'baseui/button';
+import { Skeleton } from 'baseui/skeleton';
+import { useMemo } from 'react';
+import { Clock } from 'react-feather';
+
+import TimeRecordForm from './features/TimeRecordForm';
+
+import { fetchTimeRecords, partialUpdateTimeRecord } from '../../api/records';
+import addDurationStrings from '../../utils/addDurationStrings';
+import getCurrentWeek from '../../utils/getCurrentWeek';
+
+export default function RecordsList({ projects }) {
+  const queryClient = useQueryClient();
+  const {
+    refetch,
+    isError,
+    isLoading,
+    data: recordData,
+  } = useQuery({
+    queryKey: ['records'],
+    queryFn: fetchTimeRecords,
+  });
+
+  const taskMutation = useMutation({
+    mutationFn: partialUpdateTimeRecord,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['records'] });
+    },
+  });
+
+  const getCurrentWeekTotal = (records) => {
+    if (records && records.length < 1) {
+      return '00:00:00';
+    }
+
+    return records
+      .map((record) => record.duration)
+      .reduce((accumulator, currentValue) =>
+        addDurationStrings(accumulator, currentValue),
+      );
+  };
+
+  const weekIntervals = useMemo(() => {
+    if (recordData && recordData.length > 1) {
+      const dates = [];
+      recordData.forEach((record) => {
+        const { startTime } = record;
+        const [firstDayOfTheWeek] = getCurrentWeek(startTime);
+
+        if (!dates.includes(firstDayOfTheWeek.toLocaleDateString()))
+          dates.push(firstDayOfTheWeek.toLocaleDateString());
+      });
+
+      dates.forEach((date, index) => {
+        const [firstDay, lastDay] = getCurrentWeek(new Date(date));
+        dates[index] = [firstDay, lastDay];
+      });
+
+      return dates.sort((a, b) =>
+        b[0].toLocaleDateString().localeCompare(a[0].toLocaleDateString()),
+      );
+    }
+  }, [recordData]);
+
+  const handleEditTask = (data) => {
+    taskMutation.mutate({ data, queryParams: { id: data.id } });
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col space-y-4">
+        <Skeleton rows={2} width="100%" animation />
+        <Skeleton rows={6} width="100%" animation />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="min-h-full font-prompt bg-white p-4 md:grid">
+        <div className="max-w-max">
+          <div>
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+              Error
+            </h3>
+            <p className="mt-1 text-base text-gray-500">
+              Failed to load data, please try again.
+            </p>
+          </div>
+          <div className="mt-5">
+            <Button kind="secondary" size="compact" onClick={refetch}>
+              Try again
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (!isLoading && !isError && recordData.length < 1) {
+    return (
+      <div
+        role="list"
+        aria-label="comments"
+        className="flex flex-col justify-center items-center space-y-2 pt-4 text-gray-500"
+      >
+        <Clock className="h-10 w-10" />
+        <h4>No time tracked. Create a record now!</h4>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-1 flex-col space-y-4">
+      <h1>Current Week:</h1>
+
+      {weekIntervals.map(([firstDayOfTheWeek, lastDayOfTheWeek]) => (
+        <div key={firstDayOfTheWeek} className="space-y-4">
+          <div className="flex items-center justify-between">
+            <div className="text-sm">
+              {firstDayOfTheWeek.toLocaleDateString('default', {
+                month: 'long',
+                day: '2-digit',
+              })}{' '}
+              -{' '}
+              {lastDayOfTheWeek.toLocaleDateString('default', {
+                month: 'long',
+                day: '2-digit',
+              })}
+            </div>
+            <div className="text-sm">
+              <span className="text-gray-500">Week Total: </span>
+              {getCurrentWeekTotal(
+                recordData.filter((record) => {
+                  const [recordFirstDay] = getCurrentWeek(
+                    new Date(record.time_started),
+                  );
+
+                  return (
+                    firstDayOfTheWeek.toLocaleDateString() ===
+                    recordFirstDay.toLocaleDateString()
+                  );
+                }),
+              )}
+            </div>
+          </div>
+
+          <ul className="flex flex-col mb-4 space-y-4">
+            {recordData
+              .sort((a, b) => b.time_started.localeCompare(a.time_started))
+              .map((record, index) => {
+                const [recordFirstDay] = getCurrentWeek(
+                  new Date(record.time_started),
+                );
+
+                if (
+                  firstDayOfTheWeek.toLocaleDateString() ===
+                  recordFirstDay.toLocaleDateString()
+                ) {
+                  return (
+                    <li key={record.id || index}>
+                      <TimeRecordForm
+                        isEditing={true}
+                        projects={projects}
+                        initialValues={{
+                          // have to assign taskDate and startTime here; for some reason declaring them in `records.js` doesn't give accurate dates
+                          ...record,
+                          taskDate: new Date(record.time_started),
+                          startTime: new Date(record.time_started),
+                        }}
+                        onSubmitHandler={handleEditTask}
+                      />
+                    </li>
+                  );
+                }
+
+                return <></>;
+              })}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
