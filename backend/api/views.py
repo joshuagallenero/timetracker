914b444be5905b9f63185c06c088218d2b585b87
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:30 +0800

    Add initial viewset for testing

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .serializers import (
+    UserSerializer,
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    serializer_class = UserSerializer
+    queryset = serializer_class.Meta.model.objects.all().order_by("-date_joined")
+    permission_classes = [permissions.IsAuthenticated]
