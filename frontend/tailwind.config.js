fe0210c99034c33b59d58c624bdc9f789ae3a53c
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Initial Tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
