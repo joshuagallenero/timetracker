69cbf20e9ebdeee9c9b0559f7ce53a08390df73d
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Add baseweb ui config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BaseProvider } from 'baseui';
+import { LightTheme } from 'baseui/themes';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+
+import App from './App.jsx';
+
+import './index.css';
+
+const engine = new Styletron();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider
+        theme={LightTheme}
+        overrides={{
+          AppContainer: {
+            props: {
+              className: 'font-sans',
+            },
+          },
+        }}
+      >
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>,
-)
+);
