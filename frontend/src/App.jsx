8ac7cff730c867dfc782352c0dfe65328f0eb167
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Add auth config to routes; add routes for time tracking and projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,51 @@
+import { Spinner } from 'baseui/spinner';
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
   Outlet,
   Route,
   RouterProvider,
 } from 'react-router-dom';
 
+import Tracker from './pages/home/Tracker';
 import Login from './pages/Login';
+import Projects from './pages/projects/Projects';
 import Register from './pages/Register';
+import Root from './pages/Root';
+import { checkToken } from './utils/storage';
+
+function RequiresAuth({ children }) {
+  if (!checkToken()) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Outlet />}>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route
+        path="/logout"
+        element={
+          <RequiresAuth>
+            <Spinner />
+          </RequiresAuth>
+        }
+      />
+      <Route
+        element={
+          <RequiresAuth>
+            <Root />
+          </RequiresAuth>
+        }
+      >
+        <Route index path="/home" element={<Tracker />} />
+        <Route path="/projects" element={<Projects />} />
+      </Route>
       <Route
         path="/*"
         element={
