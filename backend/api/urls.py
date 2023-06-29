a79fd76742632bd26bd2c4150655af53889d09e0
Joshua Gallenero <mjjpgallenero@gmail.com>
2023-06-29 13:27:31 +0800

    Update api urls for auth endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,8 +6,12 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
+    path("auth/login", views.CustomAuthToken.as_view(), name="login"),
+    path("auth/register", views.RegisterUserAPIView.as_view(), name="register"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
